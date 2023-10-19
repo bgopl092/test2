fd9d0182be19397487e12da5a38c1afb6937d7cd
bgopl092 <125370777+bgopl092@users.noreply.github.com>
2023-10-19 22:48:16 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,55 +1,79 @@
-apt update -y
-apt upgrade -y
-apt install qemu qemu-utils wget -y
-apt install qemu-kvm -y
-apt install ovmf -y
-apt install novnc python3 websockify -y
-cd /root
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=15Lt1skHXq5yaP5-cWpYxsuv5yEywfbgP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=15Lt1skHXq5yaP5-cWpYxsuv5yEywfbgP" -O cdrom.iso && rm -rf /tmp/cookies.txt
-wget -O 'virtio-win.iso' https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.229-1/virtio-win.iso
-mkdir fdisk
-cd fdisk
-wget -O 'ChromeSetup.msi' https://github.com/kmille36/thuonghai/releases/download/1.0.0/googlechromestandaloneenterprise64.msi
-websockify -D --web=/usr/share/novnc/ --cert=novnc.pem 8080 localhost:5900
-cd /root
-qemu-img create -f raw disk.img 32G
-qemu-system-x86_64 \
--name CodespacesQemu \
--m 8G,slots=8,maxmem=16G \
--object memory-backend-ram,size=1G,id=m0 \
--object memory-backend-ram,size=1G,id=m1 \
--object memory-backend-ram,size=1G,id=m2 \
--object memory-backend-ram,size=1G,id=m3 \
--object memory-backend-ram,size=1G,id=m4 \
--object memory-backend-ram,size=1G,id=m5 \
--object memory-backend-ram,size=1G,id=m6 \
--object memory-backend-ram,size=1G,id=m7 \
--numa node,nodeid=0,memdev=m0,cpus=0 \
--numa node,nodeid=1,memdev=m1,cpus=1 \
--numa node,nodeid=1,memdev=m2,cpus=2 \
--numa node,nodeid=1,memdev=m3,cpus=3 \
--numa node,nodeid=1,memdev=m4,cpus=4 \
--numa node,nodeid=1,memdev=m5,cpus=5 \
--numa node,nodeid=1,memdev=m6,cpus=6 \
--numa node,nodeid=1,memdev=m7,cpus=7 \
--smp 8,threads=1,cores=8,sockets=1 \
--cpu host,hv_relaxed,hv_spinlocks=8191,hv_vapic,hv_time,hv_synic,hv_stimer,hv_vpindex,hv_reset,hv_frequencies,+nx \
--enable-kvm \
--drive file=/root/virtio-win.iso,media=cdrom \
--drive file=/root/cdrom.iso,media=cdrom \
--drive file=/root/disk.img,media=disk,format=raw,if=virtio,cache=none,aio=native \
--drive file=fat:rw:/root/fdisk,media=disk,format=raw
--device usb-ehci,id=usb \
--device usb-tablet \
--device usb-kbd \
--object rng-random,filename=/dev/random,id=rng0 \
--device virtio-rng-pci,rng=rng0 \
--device intel-iommu,intremap=on,device-iotlb=on \
--device ioh3420,id=pcie.1,chassis=1 \
--vga virtio \
--machine q35,accel=kvm:tcg,kernel-irqchip=split \
--rtc base=localtime,clock=vm \
--device virtio-net-pci,bus=pcie.1,netdev=net0,disable-legacy=on,disable-modern=off,iommu_platform=on,ats=on \
--bios /usr/share/ovmf/OVMF.fd \
--netdev user,id=net0 \
--vnc :0
+#!/bin/bash
+
+arch=`uname -m`
+dest_kernel="hello-vmlinux.bin"
+dest_rootfs="hello-rootfs.ext4"
+image_bucket_url="https://s3.amazonaws.com/spec.ccfc.min/img/quickstart_guide/$arch"
+
+if [ ${arch} = "x86_64" ]; then
+    kernel="${image_bucket_url}/kernels/vmlinux.bin"
+    rootfs="${image_bucket_url}/rootfs/bionic.rootfs.ext4"
+elif [ ${arch} = "aarch64" ]; then
+    kernel="${image_bucket_url}/kernels/vmlinux.bin"
+    rootfs="${image_bucket_url}/rootfs/bionic.rootfs.ext4"
+else
+    echo "Cannot run firecracker on $arch architecture!"
+    exit 1
+fi
+
+echo "Downloading $kernel..."
+curl -fsSL -o $dest_kernel $kernel
+
+echo "Downloading $rootfs..."
+curl -fsSL -o $dest_rootfs $rootfs
+
+echo "Saved kernel file to $dest_kernel and root block device to $dest_rootfs."
+
+
+echo "Starting firecracker"
+firecracker --api-sock /tmp/firecracker.socket &
+firecracker_pid=$!
+
+rch=`uname -m`
+kernel_path=$(pwd)"/hello-vmlinux.bin"
+
+if [ ${arch} = "x86_64" ]; then
+    curl --unix-socket /tmp/firecracker.socket -i \
+      -X PUT 'http://localhost/boot-source'   \
+      -H 'Accept: application/json'           \
+      -H 'Content-Type: application/json'     \
+      -d "{
+            \"kernel_image_path\": \"${kernel_path}\",
+            \"boot_args\": \"console=ttyS0 reboot=k panic=1 pci=off\"
+       }"
+elif [ ${arch} = "aarch64" ]; then
+    curl --unix-socket /tmp/firecracker.socket -i \
+      -X PUT 'http://localhost/boot-source'   \
+      -H 'Accept: application/json'           \
+      -H 'Content-Type: application/json'     \
+      -d "{
+            \"kernel_image_path\": \"${kernel_path}\",
+            \"boot_args\": \"keep_bootcon console=ttyS0 reboot=k panic=1 pci=off\"
+       }"
+else
+    echo "Cannot run firecracker on $arch architecture!"
+    exit 1
+fi
+
+rootfs_path=$(pwd)"/hello-rootfs.ext4"
+curl --unix-socket /tmp/firecracker.socket -i \
+  -X PUT 'http://localhost/drives/rootfs' \
+  -H 'Accept: application/json'           \
+  -H 'Content-Type: application/json'     \
+  -d "{
+        \"drive_id\": \"rootfs\",
+        \"path_on_host\": \"${rootfs_path}\",
+        \"is_root_device\": true,
+        \"is_read_only\": false
+   }"
+
+curl --unix-socket /tmp/firecracker.socket -i \
+  -X PUT 'http://localhost/actions'       \
+  -H  'Accept: application/json'          \
+  -H  'Content-Type: application/json'    \
+  -d '{
+      "action_type": "InstanceStart"
+   }'
+
+sleep 20
+kill -9 $firecracker_pid
